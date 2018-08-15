a81af9fdf037ebbe93b4fe622412cd2802759e8f
Dan <dandalion98@gmail.com>
2018-08-15 12:46:52 -0700

    Add config templates

---
+++ config/env/development_sample.js
@@ -0,0 +1,15 @@
+// Fill in requred account info and rename file to development.js
+// For additional configuration options, please see README
+module.exports = {
+    stellarServer: "https://horizon-testnet.stellar.org",
+    env: "dev",
+
+    srcAccount: {
+        // address: "<your_source_account>", 
+        // seed: "<your_source_seed>"        
+    },
+
+    targetAccount: {
+        // address: "<target_account>"
+    }
+}
---
+++ config/env/production_sample.js
@@ -0,0 +1,15 @@
+// Fill in requred account info and rename file to production.js
+// For additional configuration options, please see README
+module.exports = {
+    stellarServer: "https://horizon.stellar.org",
+    env: "production",
+
+    srcAccount: {
+        // address: "<your_source_account>", 
+        // seed: "<your_source_seed>"        
+    },
+
+    targetAccount: {
+        // address: "<target_account>"
+    }
+}
